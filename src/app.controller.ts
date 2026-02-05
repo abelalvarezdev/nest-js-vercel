30388c93ddd90990530ee3e2d481681e2dc83f93
Abel Alvarez <abel.a.alvarez@ericsson.com>
2026-02-05 10:11:40 -0600

    update routes

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,18 @@
 import { Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller('hello')
+@Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get()
+  getHealth(): string {
+    return 'Ok';
   }
 
   @Post()
--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  // app.enableCors();
-  // app.setGlobalPrefix('');
   await app.listen(process.env.PORT ?? 3000);
 }
-export default bootstrap();
+bootstrap();
